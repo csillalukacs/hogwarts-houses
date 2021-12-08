3a550582d66b2c2f781f3eeb2fe494114dadb315
csilla <erzsebet.lk@gmail.com>
2021-12-08 21:53:00 +0100

    new room can be added with POST request

--- src/main/java/com/codecool/hogwartshouses/controller/RoomController.java
+++ src/main/java/com/codecool/hogwartshouses/controller/RoomController.java
@@ -1,10 +1,13 @@
 package com.codecool.hogwartshouses.controller;
 
+import com.codecool.hogwartshouses.model.Room;
 import com.codecool.hogwartshouses.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class RoomController {
@@ -15,5 +18,10 @@ public class RoomController {
     public String getRooms(Model model){
         model.addAttribute("rooms", roomService.getAll());
         return "rooms";
+    }
+
+    @PostMapping("/rooms")
+    public void add(@RequestBody Room room){
+        roomService.add(room);
     }
 }
--- src/main/java/com/codecool/hogwartshouses/model/Room.java
+++ src/main/java/com/codecool/hogwartshouses/model/Room.java
@@ -1,12 +1,16 @@
 package com.codecool.hogwartshouses.model;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
 public class Room {
     public static int idCounter = 0;
     private final int id = idCounter++;
-    private final String name;
+    private String name;
 
     public Room(String name) {
         this.name = name;
